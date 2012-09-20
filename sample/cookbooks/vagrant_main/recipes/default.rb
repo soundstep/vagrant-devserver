d40a51dc472dba875d16fa320585d296e6a41a8c
Romuald Quantin <romu@soundstep.com>
2012-09-20 18:42:05 +0100

    update recipe

--- sample/cookbooks/vagrant_main/recipes/default.rb
+++ sample/cookbooks/vagrant_main/recipes/default.rb
@@ -1,11 +1,11 @@
-require_recipe "build-essential"
-require_recipe "apt"
-require_recipe "apache2"
-require_recipe "apache2::mod_php5"
-require_recipe "apache2::mod_rewrite"
-require_recipe "git"
-require_recipe "npm"
-require_recipe "nodejs"
+# require_recipe "build-essential"
+# require_recipe "apt"
+# require_recipe "apache2"
+# require_recipe "apache2::mod_php5"
+# require_recipe "apache2::mod_rewrite"
+# require_recipe "git"
+# require_recipe "npm"
+# require_recipe "nodejs"
 
 #bash "hosts" do
 #  code "echo 127.0.0.1 vagrant-test >> /etc/hosts"
@@ -13,15 +13,15 @@ require_recipe "nodejs"
 
 # APACHE CONFIG
 
-execute "disable-default-site" do
-  command "sudo a2dissite default"
-  notifies :reload, resources(:service => "apache2"), :delayed
-end
+# execute "disable-default-site" do
+#   command "sudo a2dissite default"
+#   notifies :reload, resources(:service => "apache2"), :delayed
+# end
 
-web_app "application" do
-  template "application.conf.erb"
-  notifies :reload, resources(:service => "apache2"), :delayed
-end
+# web_app "application" do
+#   template "application.conf.erb"
+#   notifies :reload, resources(:service => "apache2"), :delayed
+# end
 
 # GIT CONFIG
 
@@ -36,3 +36,5 @@ cookbook_file "/home/vagrant/.gitignore" do
   source ".gitignore"
 end
 
+# activate ssh-agent, type in terminal (respect same quotes):
+# eval `ssh-agent`
