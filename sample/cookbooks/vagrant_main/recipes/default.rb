1d1706309e4778a587d1061b2b655826df6b560d
Romuald Quantin <romu@soundstep.com>
2012-09-20 14:24:40 +0100

    update recipe

--- sample/cookbooks/vagrant_main/recipes/default.rb
+++ sample/cookbooks/vagrant_main/recipes/default.rb
@@ -1,11 +1,11 @@
 require_recipe "build-essential"
 require_recipe "apt"
 require_recipe "apache2"
-# require_recipe "apache2::mod_php5"
-# require_recipe "apache2::mod_rewrite"
-# require_recipe "git"
-# require_recipe "npm"
-# require_recipe "nodejs"
+require_recipe "apache2::mod_php5"
+require_recipe "apache2::mod_rewrite"
+require_recipe "git"
+require_recipe "npm"
+require_recipe "nodejs"
 
 #bash "hosts" do
 #  code "echo 127.0.0.1 vagrant-test >> /etc/hosts"
